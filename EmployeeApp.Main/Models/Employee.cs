96fb8ab77d8e86d72f5bee7fd01d9de04c609460
User <gold.like.simple@gmail.com>
2023-05-01 18:25:47 +1000

    [+] Employee model

---
+++ EmployeeApp.Main/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿namespace EmployeeApp.Main.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public decimal SalaryPerHour { get; set; }
+
+
+    public static Employee CreateModel(string firstName, string lastName, decimal salaryPerHour)
+    {
+        return new Employee()
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            SalaryPerHour = salaryPerHour
+        };
+    }
+    
+    public void Update(string? firstName, string? lastName, decimal salary)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        SalaryPerHour = salary;
+    }
+}
